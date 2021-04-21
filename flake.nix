b6f80ccd84f065759b561c8ec1bb14bc1db7df1f
Yves Fischer <yvesf+git@xapek.org>
2021-04-21 23:43:23 +0200

    update private tax 2020

--- flake.nix
+++ flake.nix
@@ -7,7 +7,11 @@
       eduke32 = callPackage ./eduke32 {};
       jave = callPackage ./jave {};
       privateTax2019 = callPackage (import ./privateTax { version = "2019"; sha256 = "sha256-mbr+GjYBcRuI7eL9qrwCMhv8UqBKwejTUJhnZq7wXyI="; }) {};
-      privateTax2020 = callPackage (import ./privateTax { version = "2020"; sha256 = "sha256-lwL/ScG4JJR6YMA/QAgypeB61QSiSA2AJU9chjUZ8Fg="; }) {};
+      privateTax2020 = callPackage (import ./privateTax { 
+        version = "2020"; 
+        # sha256 = "sha256-lwL/ScG4JJR6YMA/QAgypeB61QSiSA2AJU9chjUZ8Fg="; outdated 2021-04-21
+        sha256 = "sha256-y2o4wMO2JJKXdnOB9Jx+4N7SkAdBffop1159hQ+YNMc=";
+      }) {};
       qelectrotech = callPackage ./qelectrotech {};
       tinygo-bin = callPackage ./tinygo-bin {};
     };
