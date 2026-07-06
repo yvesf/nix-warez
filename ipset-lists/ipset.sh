bd7644c6069a97f5a7d2a3b8c960a2f16b2cf260
Yves Fischer <yvesf+git@xapek.org>
2026-07-06 20:53:53 +0200

    update ipset-lists

--- ipset-lists/ipset.sh
+++ ipset-lists/ipset.sh
@@ -24,16 +24,19 @@ download() {
     ipset restore < <(
         echo "destroy -! tmp"
         length=$(wc -l < "${filename}")
-        echo "create tmp hash:net family ${family} hashsize $length"
+        echo "create tmp hash:net family ${family} hashsize ${length} maxelem ${length}"
         sed -n -e "s/^\([^;#]\+\).*/add tmp \1/p" "${filename}"
-
-        echo "create -exist ${name} hash:net family ${family} hashsize $length"
-        echo "swap tmp ${name}"
-        echo "destroy tmp"
     )
+    if ipset list -name "${name}" >/dev/null 2>&1; then
+        ipset swap tmp "${name}"
+        ipset destroy tmp
+    else
+        ipset rename tmp "${name}"
+    fi
 }
 
 download "inet" "firehol_level1" "https://iplists.firehol.org/files/firehol_level1.netset"
 download "inet" "firehol_level2" "https://iplists.firehol.org/files/firehol_level2.netset"
 download "inet" "firehol_level3" "https://iplists.firehol.org/files/firehol_level3.netset"
+download "inet" "firehol_level4" "https://iplists.firehol.org/files/firehol_level4.netset"
 download "inet6" "dropv6" "https://www.spamhaus.org/drop/dropv6.txt"
